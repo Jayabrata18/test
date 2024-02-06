2f7115438f2800c5c67e15e9778bab1d500048c1
Jayabrata Pramanik <jayabrata180402@gmail.com>
2024-02-06 23:35:36 +0530

    ..

--- backend/app.ts
+++ backend/app.ts
@@ -15,7 +15,7 @@ app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://test-signin-signup.vercel.app/",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
